50e65aba39262632868739f42ec8990d67066ba4
CometArao <90157839+CometArao@users.noreply.github.com>
2024-10-26 21:09:39 -0300

    Update horario_dia.entity.js

--- backend/src/entity/horario_dia.entity.js
+++ backend/src/entity/horario_dia.entity.js
@@ -1,27 +1,6 @@
 "use strict";
 import horario_laboral_schema from "./horario_laboral.entity.js"
-//import {Entity, PrimaryGeneratorColumn, Column } from "typeorm"
-//import { Entity, JoinTable, ManyToMany} from "typeorm";
 import { EntitySchema} from "typeorm";
-
-//@Entity()
-//export class horario_dia {
-    //@Column({type: "int", primary: true})
-    //id_horario_dia; 
-    //@Column({type: "int"})
-    //hora_inicio
-    //@Column({type: "int"})
-    //hora_fin
-    //@Column({type: "varchar"})
-    //dia_semana
-    //@ManyToMany(() => horario_laboral)
-    //@JoinTable()
-    //horario_laboral_atributo // : horario_labora[] //sale que es un array en la documentacion
-//}
-
-
-//}
-
 
 const horario_dia = new EntitySchema({
     name: "horario_dia",
@@ -42,14 +21,6 @@ const horario_dia = new EntitySchema({
             type: "varchar",
             lenght: "miercoles".length
         }
-        //relations: {
-            //horario_laboral_schema: {
-                //type: "many-to-many",
-                //target: "horario_laboral",
-                //JoinTable: true,
-                //cascade: true
-            //}
-        //}
     }
 })
 export default horario_dia;
