10eb8c28b41218ea8f8306ddb0fce93c8b789252
CometArao <cometarao@gmail.com>
2024-10-26 23:08:53 -0300

    Actualización de inicio server

--- backend/src/config/initialSetup.js
+++ backend/src/config/initialSetup.js
@@ -1,5 +1,5 @@
 "use strict";
-import User from "../entity/user.entity.js";
+import Usuario from "../entity/usuario.entity.js";
 import horario_laboral from "../entity/horario_laboral.entity.js";
 import horario_dia from "../entity/horario_dia.entity.js";
 import { AppDataSource } from "./configDb.js";
@@ -7,82 +7,27 @@ import { encryptPassword } from "../helpers/bcrypt.helper.js";
 
 async function createUsers() {
   try {
-    const userRepository = AppDataSource.getRepository(usuarios);
+    const usuarioRepository = AppDataSource.getRepository(Usuario);
 
-    const count = await userRepository.count();
+    const count = await usuarioRepository.count();
     if (count > 0) return;
 
     await Promise.all([
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Diego Alexis Salazar Jara",
-          rut: "21.308.770-3",
-          email: "administrador2024@gmail.cl",
-          password: await encryptPassword("admin1234"),
-          rol: "administrador",
+      usuarioRepository.save(
+        usuarioRepository.create({
+          nombre_usuario: "admin",
+          apellido_usuario: "master",
+          correo_usuario: "cometarao@gmail.com",
+          contrasena_usuario: await encryptPassword("admin1234"),
+          rol_usuario: "administrador",
         }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Diego Sebastián Ampuero Belmar",
-          rut: "21.151.897-9",
-          email: "usuario1.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        })
-      ),
-        userRepository.save(
-          userRepository.create({
-            nombreCompleto: "Alexander Benjamín Marcelo Carrasco Fuentes",
-            rut: "20.630.735-8",
-            email: "usuario2.2024@gmail.cl",
-            password: await encryptPassword("user1234"),
-            rol: "usuario",
-          }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Pablo Andrés Castillo Fernández",
-          rut: "20.738.450-K",
-          email: "usuario3.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Felipe Andrés Henríquez Zapata",
-          rut: "20.976.635-3",
-          email: "usuario4.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Diego Alexis Meza Ortega",
-          rut: "21.172.447-1",
-          email: "usuario5.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
-      userRepository.save(
-        userRepository.create({
-          nombreCompleto: "Juan Pablo Rosas Martin",
-          rut: "20.738.415-1",
-          email: "usuario6.2024@gmail.cl",
-          password: await encryptPassword("user1234"),
-          rol: "usuario",
-        }),
-      ),
+      )
     ]);
     console.log("* => Usuarios creados exitosamente");
   } catch (error) {
     console.error("Error al crear usuarios:", error);
   }
 }
-
 
 async function createHorario(horario_dia) {
   try {
