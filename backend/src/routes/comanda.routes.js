051905a7f436629f3ffb413c7c3480de75a7f72b
Pablo <pablosantoschavez2001@gmail.com>
2024-11-23 18:44:16 -0300

    modificacionrutaspermisos

--- backend/src/routes/comanda.routes.js
+++ backend/src/routes/comanda.routes.js
@@ -10,7 +10,7 @@ import {
   getComandasConPlatillosController,
 } from '../controllers/comanda.controller.js';
 import { authenticateJwt } from '../middlewares/authentication.middleware.js';
-import { isAdmin, isMesero } from '../middlewares/authorization.middleware.js';
+import {isMesero} from '../middlewares/authorization.middleware.js';
 
 const router = express.Router();
 
@@ -22,7 +22,7 @@ router.post('/', isMesero, createComandaController); // Crear una comanda
 router.get('/', isMesero, getAllComandasController); // Obtener todas las comandas
 router.get('/:id', isMesero, getComandaByIdController); // Obtener una comanda por ID
 router.put('/:id', isMesero, updateComandaController); // Actualizar una comanda
-router.delete('/:id', isAdmin, deleteComandaController); // Eliminar una comanda (solo admins)
+router.delete('/:id', isMesero, deleteComandaController); // Eliminar una comanda (solo admins)
 router.patch('/:id/complete', isMesero, completeComandaController); // Completar una comanda
 router.post('/:id/platillos', isMesero, addPlatilloToComandaController); // Añadir un platillo a una comanda
 router.get('/comandas/platillos', isMesero, getComandasConPlatillosController); // Obtener comandas con platillos
