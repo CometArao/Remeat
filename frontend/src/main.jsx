5bf44795499a1e29bd532fa512ab30bafe2d5d0b
Pablo <pablosantoschavez2001@gmail.com>
2024-12-01 01:54:59 -0300

    savedecommitbonitodesactualizadoconmain

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,11 +16,10 @@ import Informes from '@pages/Informes'
 import Grafico from './pages/Grafico.jsx';
 import Mermas from '@pages/Mermas.jsx';
 
-import Pedidos from './pages/Pedidos.jsx';
+
 import Comandas from '@pages/Comandas'; 
 import GenerateQRCode from '@pages/GenerateQRCode'; 
 import Ingredientes from '@pages/Ingredientes';
-import Ingredientes2 from '@pages/Ingredientes2';
 
 const data_lineal = [
   {
@@ -515,7 +514,7 @@ const router = createBrowserRouter([
         element: (
           <ProtectedRoute allowedRoles={['administrador']}>
             <div style={{ height: '80vh', marginTop: '10vh' }}>
-              <GraficoLineal data={data_lineal} legendX = {"test"} legendY = {"testy"}/>
+              <GraficoLineal data={data_lineal} />
             </div>
           </ProtectedRoute>
         ),
@@ -574,22 +573,6 @@ const router = createBrowserRouter([
         element: (
           <ProtectedRoute allowedRoles={['mesero']}>
             <GenerateQRCode />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: '/pedido', // Ruta para generar QR, accesible solo para mesero
-        element: (
-          <ProtectedRoute allowedRoles={['administrador']}>
-            <Pedidos />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: '/ingredientes2', // Ruta para generar QR, accesible solo para mesero
-        element: (
-          <ProtectedRoute allowedRoles={['administrador']}>
-            <Ingredientes2 />
           </ProtectedRoute>
         ),
       },
