a09d5105ba041060bc0219454e26898356ec4ab0
Pablo <pablosantoschavez2001@gmail.com>
2024-12-08 14:39:55 -0300

    borrando hooks que no se usan

--- frontend/src/services/MenuDiarioMenuQr.service.js
+++ frontend/src/services/MenuDiarioMenuQr.service.js
@@ -3,7 +3,7 @@ import axios from './root.service';
 
 export const generateMenuQRCode = async () => {
     const response = await axios.get('/menus/menu/qr', {
-      withCredentials: true, // Asegurar que las cookies se incluyan en la solicitud
+      withCredentials: true, 
     });
     return response.data;
   };
