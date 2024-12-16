002b08cde7d394c05c0adf2b63b11c55e8b39adf
CometArao <cometarao@gmail.com>
2024-12-16 01:45:33 -0300

    FIX: formateo de fechas de menus e ingredientes

--- frontend/src/hooks/ingredientes/useGetIngredientes.jsx
+++ frontend/src/hooks/ingredientes/useGetIngredientes.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getIngredientes } from '@services/ingredientes.service';
 import { useNavigate } from 'react-router-dom'; // Importar navigate
+import { formatDateDMY } from '../../../../backend/src/utils/dateUtils.js';
 
 const useGetIngredientes = () => {
     const [ingredientes, setIngredientes] = useState([]);
@@ -10,8 +11,15 @@ const useGetIngredientes = () => {
     const fetchIngredientes = async () => {
         try {
             const data = await getIngredientes();
+
             if (Array.isArray(data)) {
-                setIngredientes(data);
+                // Formatear las fechas antes de establecer el estado
+                const formattedData = data.map((ingrediente) => ({
+                    ...ingrediente,
+                    fecha_vencimiento: formatDateDMY(ingrediente.fecha_vencimiento),
+                }));
+
+                setIngredientes(formattedData);
             } else {
                 console.error("Error fetching ingredientes: ", data);
             }
--- frontend/src/hooks/menus/useGetMenus.jsx
+++ frontend/src/hooks/menus/useGetMenus.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getMenus } from '@services/menu.service.js';
+import { formatDateDMY } from '../../../../backend/src/utils/dateUtils.js';
 
 const useGetMenus = () => {
     const [menus, setMenus] = useState([]);
@@ -9,7 +10,13 @@ const useGetMenus = () => {
         try {
             const data = await getMenus();
             console.log('Data retornada por getMenus:', data);
-            setMenus(data);
+            const formattedData = data.map((menu) => ({
+                ...menu,
+                fecha: formatDateDMY(menu.fecha),
+            }));
+
+            
+            setMenus(formattedData);
             console.log('Estado menus despues de setMenus:', menus);
         } catch (error) {
             console.error('Error fetching menus:', error);
